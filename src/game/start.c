51000060a4fff16fadf6f81daa8b28e292a67ff8
Alex1um <nothingorno@gmail.com>
2022-03-19 01:52:34 +0700

    some kekw

--- src/game/start.c
+++ src/game/start.c
@@ -41,9 +41,10 @@ void sleep_ms(int milliseconds){ // cross-platform sleep function
 }
 
 #include <SDL2/SDL.h>
-void doInput(int *dx, int *dy)
+void doInput(int *dx, int *dy, ListGameObject left, ListGameObject right, Area area)
 {
   SDL_Event event;
+  int lx = 0, rx = 0;
   while (SDL_PollEvent(&event))
   {
     switch (event.type)
@@ -61,10 +62,30 @@ void doInput(int *dx, int *dy)
           *dy = -1;
         } else if (event.key.keysym.sym == SDLK_DOWN) {
           *dy = 1;
+        } else if (event.key.keysym.sym == SDLK_a) {
+          lx = -1;
+        } else if (event.key.keysym.sym == SDLK_z) {
+          lx = 1;
+        } else if (event.key.keysym.sym == SDLK_k) {
+          rx = -1;
+        } else if (event.key.keysym.sym == SDLK_m) {
+          rx = 1;
         }
         break;
       default:
         break;
+    }
+  }
+  if (rx) {
+    foreach(e, right) {
+      GameObject *ns = listItem_get(e);
+      area_GameObject_move(ns, area, 0, rx, 0);
+    }
+  }
+  if (lx) {
+    foreach(e, left) {
+      GameObject *ns = listItem_get(e);
+      area_GameObject_move(ns, area, 0, lx, 0);
     }
   }
 }
@@ -78,15 +99,21 @@ TextureStorage LoadTextures(Render *render) {
   return storage;
 }
 
-void initGame(ListGameObject players, ListGameObject nishals, Area area) {
+void initGame(ListGameObject players, ListGameObject nishals_left, ListGameObject nishals_right, Area area) {
   for (int x = 0; x < AREA_MAX_X; x++) {
     for (int y = 0; y < AREA_MAX_Y; y++) {
       createObject(Tile, x, y, 0, area, 0);
-      if ((y == 0 || y == AREA_MAX_Y - 1) && x % 2 == 0 || (x == 0 || x == AREA_MAX_X - 1) && y % 2 == 0) {
-        createObject(Nishal, x, y, 1, area, nishals);
-      }
+//      if ((y == 0 || y == AREA_MAX_Y - 1) && x % 2 == 0 || (x == 0 || x == AREA_MAX_X - 1) && y % 2 == 0) {
+//        createObject(Nishal, x, y, 1, area, nishals);
+//      }
     }
   }
+  createObject(Nishal, 1, 0, 1, area, nishals_left);
+  createObject(Nishal, 2, 1, 1, area, nishals_left);
+  createObject(Nishal, 1, 2, 1, area, nishals_left);
+  createObject(Nishal, AREA_MAX_X - 2, AREA_MAX_Y - 1, 1, area, nishals_right);
+  createObject(Nishal, AREA_MAX_X - 3, AREA_MAX_Y - 2, 1, area, nishals_right);
+  createObject(Nishal, AREA_MAX_X - 2, AREA_MAX_Y - 3, 1, area, nishals_right);
   createObject(Player, AREA_MAX_X / 2, AREA_MAX_Y / 2, 2, area, players);
 }
 
@@ -96,8 +123,9 @@ _Noreturn void start_game() {
   Area area;
   Area_init(area);
   ListGameObject players = ListGameObject_new();
-  ListGameObject nishals = ListGameObject_new();
-  initGame(players, nishals, area);
+  ListGameObject nishal_left = ListGameObject_new();
+  ListGameObject nishal_right = ListGameObject_new();
+  initGame(players, nishal_left, nishal_right, area);
   View *global_view = View_new(&area, 0, 0, AREA_MAX_X, AREA_MAX_Y);
   Screen game_screen = Screen_new(global_view);
   Render *render = Render_new(game_screen, 0, AREA_MAX_X * 3 * 6 * 5, AREA_MAX_Y * 3 * 3 * 8);
@@ -120,12 +148,19 @@ _Noreturn void start_game() {
     } else if (cy <= 0) {
       dy = 1;
     }
+    if (area_get(area, cx + dx, cy + dy, 1) != 0 || area_get(area, cx + dx, cy, 1)) {
+      if (cx > AREA_MAX_X / 2)
+        dx = -1;
+      else
+        dx = 1;
+    }
     area_GameObject_move(player, area, dx, dy, 0);
-    doInput(&dx, &dy);
+    doInput(&dx, &dy, nishal_left, nishal_right, area);
     sleep_ms(100);
   }
   list_free(players, gameObject_free);
-  list_free(nishals, gameObject_free);
+  list_free(nishal_right, gameObject_free);
+  list_free(nishal_left, gameObject_free);
   screen_free(game_screen);
   view_free(global_view);
   Render_free(render);
