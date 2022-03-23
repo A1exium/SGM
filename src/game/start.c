dacdc8439ad1f3257cc45db40b3c82997e4b2a8b
Alex1um <nothingorno@gmail.com>
2022-03-24 00:22:00 +0700

    custom events now working for sdl without arguments

--- src/game/start.c
+++ src/game/start.c
@@ -12,90 +12,65 @@ enum GameObjectType_t {
   Nishal,
 };
 
+//#include <SDL2/SDL.h>
+//void doInput(int *dx, int *dy, ListGameObject left, ListGameObject right, Area area)
+//{
+//  SDL_Event event;
+//  int lx = 0, rx = 0;
+//  while (SDL_PollEvent(&event))
+//  {
+//    switch (event.type)
+//    {
+//      case SDL_QUIT:
+//        exit(0);
+//        break;
+//      case SDL_KEYDOWN:
+//        fflush(stdout);
+//        if (event.key.keysym.sym == SDLK_LEFT) {
+//          *dx = -1;
+//        } else if (event.key.keysym.sym == SDLK_RIGHT) {
+//          *dx = 1;
+//        } else if (event.key.keysym.sym == SDLK_UP) {
+//          *dy = -1;
+//        } else if (event.key.keysym.sym == SDLK_DOWN) {
+//          *dy = 1;
+//        } else if (event.key.keysym.sym == SDLK_a) {
+//          lx = -1;
+//        } else if (event.key.keysym.sym == SDLK_z) {
+//          lx = 1;
+//        } else if (event.key.keysym.sym == SDLK_k) {
+//          rx = -1;
+//        } else if (event.key.keysym.sym == SDLK_m) {
+//          rx = 1;
+//        }
+//        break;
+//      default:
+//        break;
+//    }
+//  }
+//  if (rx) {
+//    foreach(e, right) {
+//      GameObject *ns = listItem_get(e);
+//      area_GameObject_move(ns, area, 0, rx, 0);
+//    }
+//  }
+//  if (lx) {
+//    foreach(e, left) {
+//      GameObject *ns = listItem_get(e);
+//      area_GameObject_move(ns, area, 0, lx, 0);
+//    }
+//  }
+//}
 
-#ifdef __EMSCRIPTEN__
-#include <emscripten.h>
-#elif WIN32
-#include <windows.h>
-#elif _POSIX_C_SOURCE >= 199309L
-#include <time.h>   // for nanosleep
-#else
-#include <unistd.h> // for usleep
-#endif
-
-void sleep_ms(int milliseconds){ // cross-platform sleep function
-#ifdef __EMSCRIPTEN__
-  emscripten_sleep(milliseconds);
-#elif WIN32
-  Sleep(milliseconds);
-#elif _POSIX_C_SOURCE >= 199309L
-  struct timespec ts;
-  ts.tv_sec = milliseconds / 1000;
-  ts.tv_nsec = (milliseconds % 1000) * 1000000;
-  nanosleep(&ts, NULL);
-#else
-  if (milliseconds >= 1000)
-      sleep(milliseconds / 1000);
-    usleep((milliseconds % 1000) * 1000);
-#endif
-}
-
-#include <SDL2/SDL.h>
-void doInput(int *dx, int *dy, ListGameObject left, ListGameObject right, Area area)
-{
-  SDL_Event event;
-  int lx = 0, rx = 0;
-  while (SDL_PollEvent(&event))
-  {
-    switch (event.type)
-    {
-      case SDL_QUIT:
-        exit(0);
-        break;
-      case SDL_KEYDOWN:
-        fflush(stdout);
-        if (event.key.keysym.sym == SDLK_LEFT) {
-          *dx = -1;
-        } else if (event.key.keysym.sym == SDLK_RIGHT) {
-          *dx = 1;
-        } else if (event.key.keysym.sym == SDLK_UP) {
-          *dy = -1;
-        } else if (event.key.keysym.sym == SDLK_DOWN) {
-          *dy = 1;
-        } else if (event.key.keysym.sym == SDLK_a) {
-          lx = -1;
-        } else if (event.key.keysym.sym == SDLK_z) {
-          lx = 1;
-        } else if (event.key.keysym.sym == SDLK_k) {
-          rx = -1;
-        } else if (event.key.keysym.sym == SDLK_m) {
-          rx = 1;
-        }
-        break;
-      default:
-        break;
-    }
-  }
-  if (rx) {
-    foreach(e, right) {
-      GameObject *ns = listItem_get(e);
-      area_GameObject_move(ns, area, 0, rx, 0);
-    }
-  }
-  if (lx) {
-    foreach(e, left) {
-      GameObject *ns = listItem_get(e);
-      area_GameObject_move(ns, area, 0, lx, 0);
-    }
-  }
-}
+//#define IMG_EXT "png"
+//extern const EventCallbackArgs NO_ARGS;
 
 TextureStorage LoadTextures(Render *render) {
   TextureStorage storage = TextureStorage_new(4);
-  textureStorage_insert(storage, Player, Texture_load(render, "assets/player"));
-  textureStorage_insert(storage, Nishal, Texture_load(render, "assets/nishal"));
-  textureStorage_insert(storage, Tile, Texture_load(render, "assets/tile"));
-  textureStorage_insert(storage, None, Texture_load(render, "assets/none"));
+  textureStorage_insert(storage, Player, Texture_load(render, get_asset_path("player")));
+  textureStorage_insert(storage, Nishal, Texture_load(render, get_asset_path("nishal")));
+  textureStorage_insert(storage, Tile, Texture_load(render, get_asset_path("tile")));
+  textureStorage_insert(storage, None, Texture_load(render, get_asset_path("none")));
   return storage;
 }
 
@@ -117,7 +92,25 @@ void initGame(ListGameObject players, ListGameObject nishals_left, ListGameObjec
   createObject(Player, AREA_MAX_X / 2, AREA_MAX_Y / 2, 2, area, players);
 }
 
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+void print_e(void *_) {
+  SDL_Log("12312");
+  printf("213213");
+}
+
+const EventCallbackArgs NO_ARGS = {
+    .length = 0,
+    .storage = 0,
+};
+
+Render *GLOBAL_RENDER;
+
 _Noreturn void start_game() {
+  EventPool_create();
+  ListeningTable_init();
+//  printf("213213");
 //  SetConsoleMode()
   initCurrentRender();
   Area area;
@@ -133,36 +126,50 @@ _Noreturn void start_game() {
   render_set_textureStorage(render, textures);
   GameObject *player = listItem_get(list_first(players));
 
-  int dx = 1, dy = 1;
-  while (1) {
-    render_render(render);
-    Position player_pos = gameObject_get_pos(player);
-    int cx = player_pos.x, cy = player_pos.y;
-    if (cx >= AREA_MAX_X - 1) {
-      dx = -1;
-    } else if (cx <= 0) {
-      dx = 1;
-    }
-    if (cy >= AREA_MAX_Y - 1) {
-      dy = -1;
-    } else if (cy <= 0) {
-      dy = 1;
-    }
-    if (area_get(area, cx + dx, cy + dy, 1) != 0 || area_get(area, cx + dx, cy, 1)) {
-      if (cx > AREA_MAX_X / 2)
-        dx = -1;
-      else
-        dx = 1;
-    }
-    area_GameObject_move(player, area, dx, dy, 0);
-    doInput(&dx, &dy, nishal_left, nishal_right, area);
-    sleep_ms(100);
-  }
-  list_free(players, gameObject_free);
-  list_free(nishal_right, gameObject_free);
-  list_free(nishal_left, gameObject_free);
-  screen_free(game_screen);
-  view_free(global_view);
-  Render_free(render);
-  textureStorage_free(textures);
+//  Event loop;
+//  loop.type = LoopEvent;
+//  addEventListener(loop, print_e, NO_ARGS);
+//  GLOBAL_RENDER = render;
+
+  Event key;
+  key.type = Keyboard;
+  key.key = 'l';
+  addEventListener(key, print_e, NO_ARGS);
+  GLOBAL_RENDER = render;
+
+//  int dx = 1, dy = 1;
+//  while (1) {
+//    print_e(0);
+//    render_render(render);
+//    Position player_pos = gameObject_get_pos(player);
+//    int cx = player_pos.x, cy = player_pos.y;
+//    if (cx >= AREA_MAX_X - 1) {
+//      dx = -1;
+//    } else if (cx <= 0) {
+//      dx = 1;
+//    }
+//    if (cy >= AREA_MAX_Y - 1) {
+//      dy = -1;
+//    } else if (cy <= 0) {
+//      dy = 1;
+//    }
+//    if (area_get(area, cx + dx, cy + dy, 1) != 0 || area_get(area, cx + dx, cy, 1)) {
+//      if (cx > AREA_MAX_X / 2)
+//        dx = -1;
+//      else
+//        dx = 1;
+//    }
+//    area_GameObject_move(player, area, dx, dy, 0);
+////    doInput(&dx, &dy, nishal_left, nishal_right, area);
+//    sleep_ms(100);
+//  }
+
+//  list_free(players, gameObject_free);
+//  list_free(nishal_right, gameObject_free);
+//  list_free(nishal_left, gameObject_free);
+//  screen_free(game_screen);
+//  view_free(global_view);
+//  Render_free(render);
+//  textureStorage_free(textures);
+  startEventLoop();
 }
--- src/main.c
+++ src/main.c
@@ -1,21 +1,22 @@
 //
 // Created by alexium on 11.02.2022.
 //
-#include "./engine/renders/2D/terminal/console.h"
-#include "engine/objects/mod.h"
-#include "engine/renders/2D/base/View.h"
-#include "engine/renders/2D/base/Render.h"
+//#include "engine.h"
 
 // ADMIN LOH
 // Second dev tozhe loh.
 
 _Noreturn extern void start_game();
 
+
 int main() {
 //#ifdef __EMSCRIPTEN__
 //#include <emscripten.h>
 //  emscripten_set_main_loop(start_game, 60, 1);
 //#else
+//  EventPool_create();
+//  ListeningTable_init();
   start_game();
+//  startEventLoop();
 //#endif
 }
